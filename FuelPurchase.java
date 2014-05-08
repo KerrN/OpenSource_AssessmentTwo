01b3eb920245b90959584424709dad0a5495580d
041401117 <041401117@my.central.wa.edu.au>
2014-05-08 14:51:22 +0800

    Update FuelPurchase.java

--- FuelPurchase.java
+++ FuelPurchase.java
@@ -14,3 +14,42 @@ public class FuelPurchase {
 		this.fuelEconomy = fuelEconomy;
 	}
 }
+
+
+/*By Jingxian And Connie*/
+package org.jx.test;
+
+public class FuelPurchase {
+	private double costPerlitre;
+	private double fuelLitres;
+
+	public FuelPurchase(double fuelLitres, double costPerlitre) {
+		this.costPerlitre = costPerlitre;
+		this.fuelLitres = fuelLitres;
+	}
+
+	public double getTotalFuelCost() {
+		return this.costPerlitre * fuelLitres;
+	}
+
+	public double getFuelEconomy() {
+		return this.fuelLitres / 100;
+	}
+
+	public double getCostPerlitre() {
+		return costPerlitre;
+	}
+
+	public void setCostPerlitre(double costPerlitre) {
+		this.costPerlitre = costPerlitre;
+	}
+
+	public double getFuelLitres() {
+		return fuelLitres;
+	}
+
+	public void setFuelLitres(double fuelLitres) {
+		this.fuelLitres = fuelLitres;
+	}
+
+}
