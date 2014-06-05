279191b3d1c18e53844d12d2ad105d2635b84742
Titan501 <jordancoombs@ymail.com>
2014-06-05 14:44:32 +0800

    Update Main.java
    added new vehicle- Jordan coombs

--- Main.java
+++ Main.java
@@ -8,17 +8,19 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Vehicle v = new Vehicle("Ford", "T812", 2014);
-
+                Vehicle x = new Vehicle("Dmc","Delorian", 1980); //added new car
 		v.printDetails();
-		
+		x.printDetails();
 		// Vehicle sample distance
 		for (int i = 0; i < 10; i++) {
 			v.addKilometers(new Random().nextInt(100));
+                        x.addKilometers(new Random().nextInt(100)); //added new int
 		}
 		
 		System.out.println("\n\n");
 		
 		v.printDetails();
-	}
+                x.printDetails(); //and new print
+	} 
 
 }
